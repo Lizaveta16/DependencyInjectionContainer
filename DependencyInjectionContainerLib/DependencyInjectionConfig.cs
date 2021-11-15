1ad60703ac9d94cc67ce62df10c726550b9ddda0
Lizaveta <lizazaytseva16@gmail.com>
2021-11-15 16:26:11 +0300

    Added method to add dependencies to the configuration

--- DependencyInjectionContainerLib/DependencyInjectionConfig.cs
+++ DependencyInjectionContainerLib/DependencyInjectionConfig.cs
@@ -19,6 +19,25 @@ namespace DependencyInjectionContainerLib
                 _interface.IsAssignableFrom(_implementation))
             {
                 var registeredType = new RegisteredTypeInfo(_interface, _implementation, _lifecycleType);
+                if (!_registeredTypes.TryGetValue(_interface, out List<RegisteredTypeInfo> typesAlreadyRegistered))
+                {
+                    _registeredTypes.Add(_interface, new List<RegisteredTypeInfo>() { registeredType });
+                }
+                else
+                {
+                    if (!typesAlreadyRegistered.Contains(registeredType))
+                    {
+                        typesAlreadyRegistered.Add(registeredType);
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            }
+            else
+            {
+              
             }
         }
     }
